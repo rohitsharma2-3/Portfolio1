e138884518c4b342b1beda1c075913a2787cb9d4
rohitsharma2-3 <rohit.sharma2k2k@gmail.com>
2025-07-01 17:46:18 +0530

    Initial Commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,9 +9,8 @@ const Hero = () => {
             <img src={Rohit} alt="Rohit Sharma" className="w-32  rounded-full object-cover mx-auto" />
             <h5 className="text-2xl font-semibold">Hi! I'm Rohit Sharma</h5>
             <p className='mt-3 mb-3 font-bold text-1xl md:text-4xl'>MERN Stack Developer.</p>
-            <p className='text-gray-500'>MERN Stack developer with experience building and deploying scalable web applications. Proficient in
-                React, Node.js, MongoDB, and RESTful APIs. Strong foundation in frontend design with Tailwind CSS and
-                Git version control. Eager to contribute to fast-paced development teams and grow professionally.</p>
+            <p className='text-gray-500'>I build modern web apps using the MERN stack. <br className='hidden md:block' />
+           Focused on clean code, performance, and great UX.</p>
             <div className='mt-5 flex gap-10 '>
                 <p className='bg-blue-500 text-white hover:text-black py-2 px-6 hover:bg-white hover:border border-black rounded'><AnchorLink className='anchor-link' offset={50} href='#contact'>Contact</AnchorLink></p>
                 <a href="Rohit_Resume.docx" download className='bg-white border border-black rounded py-2 px-6  hover:bg-blue-500 hover:text-white'>Resume</a>
